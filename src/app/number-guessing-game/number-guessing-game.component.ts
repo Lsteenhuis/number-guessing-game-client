ce0590ecc2a6b376d6e7a0ef043009b6d9991984
Lars Steenhuis <Lars.Steenhuis-CIC.Netherlands@ibm.com>
2021-03-16 20:57:28 +0100

    added number-guessing-game component

---
+++ src/app/number-guessing-game/number-guessing-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-number-guessing-game',
+  templateUrl: './number-guessing-game.component.html',
+  styleUrls: ['./number-guessing-game.component.scss']
+})
+export class NumberGuessingGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
